48b25fdbac980357a1d78413ff79413c90c44adf
rjb3vp <rjb3vp@virginia.edu>
2019-12-03 14:56:28 -0500

    more pins

--- ledExample.py
+++ ledExample.py
@@ -3,11 +3,20 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 redPin = 21
+yellowPin = 20
+bluePin = 16
 GPIO.setup(redPin,GPIO.OUT)
+GPIO.setup(yellowPin,GPIO.OUT)
+GPIO.setup(bluePin,GPIO.OUT)
+
+GPIO.output(redPin,GPIO.LOW)
+GPIO.output(yellowPin,GPIO.LOW)
+GPIO.output(bluePin,GPIO.LOW)
+
 while True:
-	print "LED on"
-	GPIO.output(redPin,GPIO.HIGH)
+	print("LED on")
+	GPIO.output(bluePin,GPIO.HIGH)
 	time.sleep(1)
-	print "LED off"
-	GPIO.output(redPin,GPIO.LOW)
+	print("LED off")
+	GPIO.output(bluePin,GPIO.LOW)
 	time.sleep(1)
